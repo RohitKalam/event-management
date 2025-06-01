f1252279dca63e732ee9583283122ecd9f19983b
Rohit <rohitkalampatil@gmail.com>
2025-06-01 21:14:41 +0530

    go

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 createRoot(document.getElementById('root')).render(
  
-    <BrowserRouter basename='/event-management'>
+    <BrowserRouter >
       <App />
     </BrowserRouter>
  
